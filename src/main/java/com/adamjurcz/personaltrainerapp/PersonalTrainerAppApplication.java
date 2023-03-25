6ad6c218c15c70fc4c349f0ea76c9f77a84f28ab
coolarrows <coolarrows@ubuntu.myguest.virtualbox.org>
2023-03-25 15:25:05 +0100

    Initial commit

---
+++ src/main/java/com/adamjurcz/personaltrainerapp/PersonalTrainerAppApplication.java
@@ -0,0 +1,13 @@
+package com.adamjurcz.personaltrainerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTrainerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalTrainerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adamjurcz/personaltrainerapp/PersonalTrainerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamjurcz.personaltrainerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTrainerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
