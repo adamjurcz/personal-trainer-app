e3895f302f69a534c2da15d33c62200654cf937a
AdamJurcz <adamos00@op.pl>
2023-05-06 22:16:35 +0200

    Implemented user profile controller

--- src/main/java/com/adamjurcz/personaltrainerapp/presenters/rest/userprofiles/UserProfileController.java
+++ src/main/java/com/adamjurcz/personaltrainerapp/presenters/rest/userprofiles/UserProfileController.java
@@ -1,14 +1,12 @@
 package com.adamjurcz.personaltrainerapp.presenters.rest.userprofiles;
 
-import com.adamjurcz.personaltrainerapp.core.domain.UserProfile;
+import com.adamjurcz.personaltrainerapp.core.domain.Identity;
 import com.adamjurcz.personaltrainerapp.core.usecases.userprofiles.AddUserProfileUseCase;
+import com.adamjurcz.personaltrainerapp.core.usecases.userprofiles.GetUserProfileUseCase;
 import com.adamjurcz.personaltrainerapp.presenters.rest.entities.RequestCreateUser;
 import com.adamjurcz.personaltrainerapp.presenters.rest.entities.ResponseUser;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(UserProfileController.endpoint)
@@ -16,9 +14,12 @@ public class UserProfileController {
     public static final String endpoint = "/api/v1/users";
 
     private final AddUserProfileUseCase addUserProfileUseCase;
+    private final GetUserProfileUseCase getUserProfileUseCase;
 
-    public UserProfileController(AddUserProfileUseCase addUserProfileUseCase) {
+    public UserProfileController(AddUserProfileUseCase addUserProfileUseCase,
+                                 GetUserProfileUseCase getUserProfileUseCase) {
         this.addUserProfileUseCase = addUserProfileUseCase;
+        this.getUserProfileUseCase = getUserProfileUseCase;
     }
 
     @PostMapping
@@ -26,5 +27,11 @@ public class UserProfileController {
          AddUserProfileUseCase.Input input= new AddUserProfileUseCase.Input(requestCreateUser.getUsername(),
                  requestCreateUser.getEmail(), requestCreateUser.getPassword());
          return ResponseUser.mapToResponse(addUserProfileUseCase.execute(input).getUserProfile());
+    }
+
+    @GetMapping("/{userId}")
+    ResponseUser getUserById(@PathVariable Long userId){
+        GetUserProfileUseCase.Input input = new GetUserProfileUseCase.Input(new Identity(userId));
+        return ResponseUser.mapToResponse(getUserProfileUseCase.execute(input).getUserProfile());
     }
 }
